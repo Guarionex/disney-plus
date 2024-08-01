11734b5ca240d728b6483556c3ef330243a4b796
Edwin Alt Karaya <chespirana@gmail.com>
2024-08-01 05:36:53 +0000

    Wrote more test for data endpoint

--- api.js
+++ api.js
@@ -1,4 +1,10 @@
 export const fetchHomePageData = () => {
   return fetch('https://cd-static.bamgrid.com/dp-117731241344/home.json')
-    .then(response => response.json())
+    .then(response => {
+      if(response.ok) {
+        return response.json()
+      } else {
+        throw new Error('Could not fetch home data')
+      }
+    })
 }
--- tests/api.spec.js
+++ tests/api.spec.js
@@ -1,9 +1,50 @@
 import {fetchHomePageData} from '../api'
 
+const mockFetch = (requestUrl) => {
+  global.fetch = jest.fn((url) => {
+    if (url === requestUrl) {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({
+          data: {
+            StandardCollection: {
+              containers: [
+                {set: {}},
+                {set: {}}
+              ]
+            }
+          }
+        })
+      })
+    } else {
+      return Promise.resolve({
+        ok: false,
+        status: 400,
+        json: () => Promise.resolve({})
+      })
+    }
+  })
+}
+
 describe('API', () => {
+  const validUrl = 'https://cd-static.bamgrid.com/dp-117731241344/home.json'
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
   it('Should fetch home page data', () => {
+    mockFetch(validUrl)
+
     fetchHomePageData().then((data) => {
-      expect(data).toBeDefined()
+      expect(data.data.StandardCollection.containers).toBeInstanceOf(Array)
+      expect(data.data.StandardCollection.containers).toHaveLength(2)
     })
+  })
+
+  it('Should handle errors when fetching home page data', () => {
+    mockFetch("not a valid url")
+
+    return expect(fetchHomePageData()).rejects.toThrow('Could not fetch home data')
   })
 })
